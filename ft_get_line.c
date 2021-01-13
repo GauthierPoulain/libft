2bd523b9fa2d66b2f8096494a8533b04918346e7
gapoulai <gapoulai@student.42lyon.fr>
2021-01-13 13:00:31 -0800

    rename beacause i'm un gros con

--- ft_get_line.c
+++ ft_get_line.c
@@ -6,13 +6,13 @@
 /*   By: gapoulai <gapoulai@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/13 12:51:52 by gapoulai          #+#    #+#             */
-/*   Updated: 2021/01/13 12:55:04 by gapoulai         ###   ########lyon.fr   */
+/*   Updated: 2021/01/13 13:00:21 by gapoulai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*get_get_line(char **tab, char *start, char *end)
+char	*ft_get_line(char **tab, char *start, char *end)
 {
 	int		i;
 
