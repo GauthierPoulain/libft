d3deffccdd3556d3d2affb66b37a6c3d22077f56
gauthier poulain <gapoulai@student.42lyon.fr>
2021-01-15 12:42:17 +0100

    add tab functions

--- ft_add_line.c
+++ ft_add_line.c
@@ -1,37 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_read_file.c                                     :+:      :+:    :+:   */
+/*   ft_add_line.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gapoulai <gapoulai@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/11 06:04:43 by gapoulai          #+#    #+#             */
-/*   Updated: 2021/01/15 12:38:45 by gapoulai         ###   ########lyon.fr   */
+/*   Created: 2021/01/15 12:37:34 by gapoulai          #+#    #+#             */
+/*   Updated: 2021/01/15 12:42:05 by gapoulai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char		**ft_read_file(int fd)
+char	**ft_add_line(char **src, char *line)
 {
-	char	*line;
-	char	**file;
+	char	**new;
+	int		j;
 	int		i;
-	int		ret;
 
-	i = 0;
-	ret = 1;
-	while (ret)
+	i = ft_tab_len(src);
+	new = ft_calloc(i + 2, sizeof(char *));
+	if (!new)
+		return (NULL);
+	j = 0;
+	while (j < i)
 	{
-		ret = ft_gnl(fd, &line);
-		if (ret == -1)
-			return (NULL);
-		if (*line)
-			file = ft_add_line(file, line);
-		else
-			free(line);
-		if (!file)
-			return (NULL);
+		new[j] = ft_strdup(src[j]);
+		free(src[j++]);
 	}
-	return (file);
+	new[j++] = ft_strdup(line);
+	new[j] = NULL;
+	free(line);
+	if (i)
+		free(src);
+	return (new);
 }
--- ft_tab_len.c
+++ ft_tab_len.c
@@ -1,37 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_read_file.c                                     :+:      :+:    :+:   */
+/*   ft_tab_len.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gapoulai <gapoulai@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/11 06:04:43 by gapoulai          #+#    #+#             */
-/*   Updated: 2021/01/15 12:38:45 by gapoulai         ###   ########lyon.fr   */
+/*   Created: 2021/01/15 12:39:18 by gapoulai          #+#    #+#             */
+/*   Updated: 2021/01/15 12:41:18 by gapoulai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char		**ft_read_file(int fd)
+size_t	ft_tab_len(char **tab)
 {
-	char	*line;
-	char	**file;
-	int		i;
-	int		ret;
+	size_t	i;
 
 	i = 0;
-	ret = 1;
-	while (ret)
-	{
-		ret = ft_gnl(fd, &line);
-		if (ret == -1)
-			return (NULL);
-		if (*line)
-			file = ft_add_line(file, line);
-		else
-			free(line);
-		if (!file)
-			return (NULL);
-	}
-	return (file);
+	while (tab[i])
+		i++;
+	return (i);
 }
