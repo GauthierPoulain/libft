dbd4d4b64fa1d5d5a8bd64a209ed1c02b2393d90
gauthier poulain <gapoulai@student.42lyon.fr>
2021-02-04 13:58:47 +0100

    salut

--- ft_rgbtohex.c
+++ ft_rgbtohex.c
@@ -6,7 +6,7 @@
 /*   By: gapoulai <gapoulai@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 09:20:50 by gapoulai          #+#    #+#             */
-/*   Updated: 2021/02/01 10:44:24 by gapoulai         ###   ########lyon.fr   */
+/*   Updated: 2021/02/04 13:52:44 by gapoulai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int			ft_rgbtohex(int r, int g, int b)
 
 int			ft_rgbatohex(int r, int g, int b, double a)
 {
-	r *= a;
-	g *= a;
-	b *= a;
+	r = ft_math_min(255, r * a);
+	g = ft_math_min(255, g * a);
+	b = ft_math_min(255, b * a);
 	return (((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff));
 }
 
@@ -31,9 +31,9 @@ int			ft_hextodimhex(int hex, double a)
 	int		g;
 	int		b;
 
-	r = hex >> 16;
-	g = (hex & 0x00ff00) >> 8;
-	b = (hex & 0x0000ff);
+	r = ft_math_min(255, hex >> 16);
+	g = ft_math_min(255, (hex & 0x00ff00) >> 8);
+	b = ft_math_min(255, (hex & 0x0000ff));
 	return (ft_rgbatohex(r, g, b, a));
 }
 
@@ -41,8 +41,8 @@ t_rgb		ft_hextorgb(int hex)
 {
 	t_rgb	rgb;
 
-	rgb.r = hex >> 16;
-	rgb.g = (hex & 0x00ff00) >> 8;
-	rgb.b = (hex & 0x0000ff);
+	rgb.r = ft_math_min(255, hex >> 16);
+	rgb.g = ft_math_min(255, (hex & 0x00ff00) >> 8);
+	rgb.b = ft_math_min(255, (hex & 0x0000ff));
 	return (rgb);
 }
