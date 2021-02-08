98fc6fdf4ac633799de69696259eab98fa5a4a57
gapoulai <gapoulai@student.42lyon.fr>
2021-02-08 14:04:20 -0800

    libv2

--- Makefile
+++ Makefile
@@ -29,122 +29,34 @@ CFLAGS = -Wall -Wextra -Werror -fno-builtin -O3
 
 MAKE = make --no-print-directory
 
-HEADER = ./libft.h
+HEADER = libft.h
 
 OBJS = $(SRCS:%.c=%.o)
-SRCS = \
-	ft_atoi.c \
-	ft_atoi_base.c \
-	ft_bzero.c  \
-	ft_calloc.c \
-	ft_convert_base.c \
-	ft_count.c \
-	ft_get_index.c \
-	ft_isalnum.c \
-	ft_isalpha.c \
-	ft_isascii.c  \
-	ft_ischarset.c \
-	ft_isdigit.c \
-	ft_isprint.c  \
-	ft_isspace.c \
-	ft_itoa.c \
-	ft_lstnew.c \
-	ft_lstadd_front.c \
-	ft_lstsize.c \
-	ft_lstlast.c \
-	ft_lstadd_back.c \
-	ft_lstdelone.c \
-	ft_lstclear.c \
-	ft_lstiter.c \
-	ft_lstmap.c \
-	ft_math_factorial.c \
-	ft_math_fibonacci.c \
-	ft_math_isprime.c \
-	ft_math_max.c \
-	ft_math_min.c \
-	ft_math_nextprime.c \
-	ft_math_power.c \
-	ft_math_sqrt.c \
-	ft_memccpy.c  \
-	ft_memchr.c \
-	ft_memcmp.c \
-	ft_memcpy.c  \
-	ft_memmove.c \
-	ft_memset.c  \
-	ft_nblen.c \
-	ft_popnbr.c \
-	ft_putchar_fd.c \
-	ft_putendl_fd.c \
-	ft_putnbr_fd.c \
-	ft_putstr_fd.c \
-	ft_realloc.c \
-	ft_rev_tab.c \
-	ft_split.c \
-	ft_strcat.c \
-	ft_strchr.c \
-	ft_strdup.c \
-	ft_strjoin.c \
-	ft_strlcat.c \
-	ft_strlcpy.c \
-	ft_strlen.c  \
-	ft_strmapi.c \
-	ft_strncmp.c \
-	ft_strndup.c \
-	ft_strnstr.c \
-	ft_strrchr.c \
-	ft_strtrim.c \
-	ft_substr.c \
-	ft_swap.c \
-	ft_tolower.c  \
-	ft_toupper.c  \
-	ft_putstrn_fd.c \
-	ft_itoa_base.c \
-	ft_uitoa_base.c \
-	ft_tolowercase.c \
-	ft_touppercase.c \
-	ft_uitoa.c \
-	ft_gnl.c \
-	ft_ctos.c \
-	ft_startwith.c \
-	ft_endwith.c \
-	ft_print_matrice.c \
-	ft_read_file.c \
-	ft_free_tab.c \
-	ft_get_line.c \
-	ft_atoi_len.c \
-	ft_rgbtohex.c \
-	ft_atof.c \
-	ft_check_limits.c \
-	ft_abs.c \
-	ft_overflow.c \
+SRCS = $(wildcard src/*.c)
 
 %.o: %.c $(HEADER)
-	@printf "[ $(_GREEN)$(_BOLD)>+$(_END) ][ compiling ] $(_BLUE)$(_BOLD)$<$(_END)\n"
+	@printf "[ ${_PURPLE}${_BOLD}${NAME}${_END} ] > [ $(_GREEN)$(_BOLD)+$(_END) ][ compiling ] $(_BLUE)$(_BOLD)$<$(_END)\n"
 	@$(CC) $(CFLAGS) -c $< -o $@
 
 all: $(NAME)
 
 $(NAME): $(OBJS)
-	@printf "[ $(_GREEN)$(_BOLD)>+$(_END) ][ building ] $(_BLUE)$(_BOLD)$(NAME)$(_END)\n"
+	@printf "[ ${_PURPLE}${_BOLD}${NAME}${_END} ] > [ $(_GREEN)$(_BOLD)+$(_END) ][ building ] $(_BLUE)$(_BOLD)$(NAME)$(_END)\n"
 	@$(AR) rcs $(NAME) $(OBJS)
-	@printf "[ $(_GREEN)$(_BOLD):)$(_END) ][ done ] $(_BLUE)$(_BOLD)$(NAME)$(_END)\n"
+	@printf "[ ${_PURPLE}${_BOLD}${NAME}${_END} ] > [ $(_BLUE)$(_BOLD)!$(_END) ][ done ]\n"
 
 re: fclean
 	@$(MAKE) all
 
 clean:
 	@$(RM) $(OBJS)
-	@printf "[ $(_RED)$(_BOLD)--$(_END) ][ removing ] $(_BLUE)$(_BOLD)libft objs $(_END)\n"
+	@printf "[ ${_PURPLE}${_BOLD}${NAME}${_END} ] > [ $(_RED)$(_BOLD)-$(_END) ][ removing ] $(_BLUE)$(_BOLD)libft objs $(_END)\n"
 
 fclean: clean
 	@$(RM) $(NAME)
-	@printf "[ $(_RED)$(_BOLD)--$(_END) ][ removing ] $(_BLUE)$(_BOLD)$(NAME) $(_END)\n"
+	@printf "[ ${_PURPLE}${_BOLD}${NAME}${_END} ] > [ $(_RED)$(_BOLD)-$(_END) ][ removing ] $(_BLUE)$(_BOLD)$(NAME) $(_END)\n"
 
 norm:
-	@norminette *.[ch]
+	@norminette
 
-test: all
-	@$(CC) $(CFLAGS) -g main.c -L. -lft -o test
-	# valgrind --leak-check=yes ./test
-
-.PHONY: all re clean fclean norm build test
+.PHONY: all re clean fclean norm
--- libft.h
+++ libft.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gapoulai <gapoulai@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/23 14:42:01 by gapoulai          #+#    #+#             */
-/*   Updated: 2021/02/07 01:36:10 by gapoulai         ###   ########lyon.fr   */
+/*   Created: 2021/02/08 06:37:55 by gapoulai          #+#    #+#             */
+/*   Updated: 2021/02/08 13:57:28 by gapoulai         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,130 +18,92 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdint.h>
+# include <stdbool.h>
 
 # define STDIN 0
 # define STDOUT 1
 # define STDERR 2
 
-typedef struct	s_list
+typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
 }				t_list;
 
-typedef struct	s_rgb
+typedef struct s_rgb
 {
-	int			r;
-	int			g;
-	int			b;
+	int		r;
+	int		g;
+	int		b;
 }				t_rgb;
 
-void			ft_bzero(void *s, size_t n);
-void			ft_rev_tab(char *tab);
-void			ft_swap(void *a, void *b);
-void			ft_tolowercase(char *s);
-void			ft_touppercase(char *s);
-void			ft_print_matrice(char **matrice);
-void			ft_free_tab(char **tab);
+typedef struct s_3dvector
+{
+	double	x;
+	double	y;
+	double	z;
+}				t_3dvector;
 
-void			*ft_calloc(size_t count, size_t size);
-void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
-void			*ft_memchr(const void *s, int c, size_t n);
-void			*ft_memcpy(void *dst, const void *src, size_t n);
-void			*ft_memmove(void *dst, const void *src, size_t len);
-void			*ft_memset(void *b, int c, size_t len);
-void			*ft_realloc(void *s, size_t size);
+typedef struct s_2dvector
+{
+	double	x;
+	double	y;
+}				t_2dvector;
 
-char			*ft_strnstr(const char *haystack, const char *needle,
-size_t len);
-char			*ft_strndup(const char *s, size_t size);
-char			*ft_strtrim(char const *s1, char const *set);
-char			*ft_strdup(const char *s1);
-char			*ft_strcat(char *dest, char *src);
-char			*ft_strrchr(const char *s, int c);
-char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-char			**ft_split(char const *s, char c);
-char			*ft_substr(char const *s, unsigned int start, size_t len);
-char			*ft_convert_base(char *nbr, int base_from, int base_to);
-char			*ft_itoa(int n);
-char			*ft_uitoa(unsigned long n);
-char			*ft_strchr(const char *s, int c);
-char			*ft_strjoin(char const *s1, char const *s2);
-char			*ft_itoa_base(unsigned int nbr, int base);
-char			*ft_uitoa_base(unsigned long nbr, int base);
-char			*ft_ctos(char c);
-char			*ft_get_line(char **tab, char *start, char *end);
-char			*ft_strjoinf1(char *s1, char *s2);
-char			*ft_strjoinf2(char *s1, char *s2);
-char			*ft_strjoinall(char *s1, char *s2);
-
-char			**ft_read_file(int fd);
-
-int				ft_tolower(int c);
-int				ft_find_next_prime(int nb);
-int				ft_math_sqrt(int nb);
-int				ft_math_factorial(int nb);
-int				ft_count(char *str, int c);
-int				ft_get_index(char *s, int c);
-int				ft_isalnum(int c);
-int				ft_isalpha(int c);
-int				ft_isascii(int c);
-int				ft_isdigit(int c);
-int				ft_ischarset(int c, char *charset);
-int				ft_isprint(int c);
-int				ft_atoi_base(char *str, int str_base);
-int				ft_isspace(int c);
-int				ft_math_fibonacci(int index);
-int				ft_math_isprime(int nb);
-int				ft_memcmp(const void *s1, const void *s2, size_t n);
-int				ft_strncmp(const char *s1, const char *s2, size_t n);
-int				ft_popnbr(int base, int next);
-int				ft_toupper(int c);
-int				ft_gnl(int fd, char **line);
-int				ft_printf(const char *s, ...);
-int				ft_fprintf(int fd, const char *s, ...);
-int				ft_startwith(char *str, char *start);
-int				ft_endwith(char *str, char *end);
-int				ft_atoi_len(const char *str);
-int				ft_rgbtohex(int r, int g, int b);
-int				ft_atof_len(char *str);
-int				ft_strcmp(const char *s1, const char *s2);
-int				ft_check_limits(long long value, long long min, long long max);
-int				ft_rgbatohex(int r, int g, int b, double a);
-int				ft_hextodimhex(int hex, double a);
-
-double			ft_atof(char *str);
-double			ft_abs(double value);
-double			ft_overflow(double min, double max, double value);
-
-long long		ft_atoi(const char *str);
-long long		ft_math_power(long long nb, long long power);
-long long		ft_math_max(long long a, long long b);
-long long		ft_math_min(long long a, long long b);
-
-size_t			ft_strlen(const char *s);
-size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t			ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t			ft_nblen(long long nb);
-size_t			ft_putchar_fd(char c, int fd);
-size_t			ft_putendl_fd(char *s, int fd);
-size_t			ft_putnbr_fd(long long n, int fd);
-size_t			ft_putstr_fd(char *s, int fd);
-size_t			ft_putstrn_fd(char *s, size_t n, int fd);
-
-void			ft_lstadd_front(t_list **alst, t_list *new);
-void			ft_lstadd_back(t_list **alst, t_list *new);
-void			ft_lstclear(t_list **lst, void (*del)(void *));
-void			ft_lstdelone(t_list *lst, void (*del)(void *));
-void			ft_lstiter(t_list *lst, void (*f)(void *));
-
-int				ft_lstsize(t_list *lst);
-
-t_list			*ft_lstlast(t_list *lst);
-t_list			*ft_lstnew(void *content);
-t_list			*ft_lstmap(t_list *lst, void *(*f)(void *),
-void (*del)(void *));
-
-t_rgb			ft_hextorgb(int hex);
+bool		ft_malloc(size_t count, size_t size, void **dst);
+bool		ft_calloc(size_t count, size_t size, void **dst);
+void		ft_bzero(void *s, size_t n);
+int			ft_atoi(const char *str);
+size_t		ft_atoi_len(const char *str);
+int			ft_atoi_base(char *str, int str_base);
+double		ft_atof(char *str);
+size_t		ft_atof_len(char *str);
+bool		ft_isalpha(char c);
+bool		ft_isdigit(char c);
+bool		ft_isalnum(char c);
+bool		ft_isascii(char c);
+bool		ft_isprint(char c);
+bool		ft_isspace(char c);
+bool		ft_ischarset(char c, char *charset);
+long long	ft_abs(long long value);
+double		ft_dabs(double value);
+long long	ft_pow(long long nb, long long power);
+size_t		ft_nblen(long long nb);
+char		*ft_strdup(char *s1);
+size_t		ft_strlen(char *s);
+bool		ft_isinrange(long long value, long long min, long long max);
+char		ft_tolower(char c);
+void		ft_tolowercase(char *s);
+char		ft_toupper(char c);
+void		ft_touppercase(char *s);
+int			ft_count(char *str, char c);
+char		*ft_ctos(char c);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_strcmp(const char *s1, const char *s2);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+char		*ft_strjoin(char *s1, char *s2);
+char		*ft_strjoinf1(char *s1, char *s2);
+char		*ft_strjoinf2(char *s1, char *s2);
+char		*ft_strjoinall(char *s1, char *s2);
+void		*ft_memcpy(void *dst, const void *src, size_t n);
+char		*ft_strcat(char *dest, char *src);
+void		ft_swap(void *a, void *b);
+int			ft_get_next_line(int fd, char **line);
+char		*ft_strjoinc(char *s1, char c);
+char		*ft_itoa(int n);
+void		ft_lstadd_back(t_list **alst, t_list *new);
+void		ft_lstadd_front(t_list **alst, t_list *new);
+void		ft_lstclear(t_list **lst, void (*del)(void *));
+void		ft_lstdelone(t_list *lst, void (*del)(void *));
+void		ft_lstiter(t_list *lst, void (*f)(void *));
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list		*ft_lstnew(void *content);
+int			ft_lstsize(t_list *lst);
+t_list		*ft_lstlast(t_list *lst);
+long long	ft_min(long long a, long long b);
+double		ft_dmin(double a, double b);
+long long	ft_max(long long a, long long b);
+double		ft_dmax(double a, double b);
+long long	ft_sqrt(long long nb);
 
 #endif
